e5855e06fb836725f428775950e96bb5f2d0b28c
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-24 21:36:35 +0100

    chore: imported expense item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import './components/ExpenseItem'
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h2>Exspense Tracker</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
