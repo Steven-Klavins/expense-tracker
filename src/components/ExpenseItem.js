209bc130c712d5c65e0b4f1524d7db02cf6fe5bc
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-26 21:34:38 +0100

    refactor: moved exspense items into card component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,17 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExspenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
-
-
   return (
-    <div className="expense-item">
-      <ExpenseDate date={props.date}/>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount} </div>
       </div>
-    </div>
+    </Card>
   );
 }
 
