109cee3bed04976071f61fda44432ee635745f39
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-26 20:54:50 +0100

    chore: added dynamic exspense data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExspenseDate";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
+
   return (
-    <div className = "expense-item">
-      <div>Date</div>
-      <div className = "expense-item__description">
-        <h2>Title</h2>
-        <div className = "expense-item__price"> Amount </div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date}/>
+
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount} </div>
       </div>
     </div>
   );
