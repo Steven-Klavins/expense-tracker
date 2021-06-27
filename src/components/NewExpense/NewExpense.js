12986663a4a1fa6567a5169807f1fd2ea1a7590a
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-27 23:13:12 +0100

    chore: added new expenae component

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NewExpense.css";
+import ExpeneseForm from "./ExpenceForm";
+
+function NewExpense() {
+  return (
+    <div className="new-expense">
+      <ExpeneseForm/>
+    </div>
+  );
+}
+
+export default NewExpense;
