7db80353309848691949629389649b634e1fedaa
Steven_Klavins <stevenklavins94@gmail.com>
2021-06-28 22:15:13 +0100

    chore: added saveExpenseDataHandler, sent to props

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./NewExpense.css";
 import ExpeneseForm from "./ExpenceForm";
+import { v4 as uuidv4 } from 'uuid';
 
-function NewExpense() {
+function NewExpense(props) {
+
+  const saveExpenseDataHandler = (enterExpenseData) => {
+    const expenseData = {
+      ...enterExpenseData,
+      id: (uuidv4())
+    };
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpeneseForm/>
+      <ExpeneseForm onSaveExpenseData = {saveExpenseDataHandler} />
     </div>
   );
 }
