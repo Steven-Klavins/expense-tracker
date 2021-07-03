384c766a35067784abfd9bd3d8cfe7e1366c7360
Steven_Klavins <stevenklavins94@gmail.com>
2021-07-03 15:23:13 +0100

    chore: passed props from expense filter

--- src/components/Expenses/ExpensesFilter/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter/ExpensesFilter.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import "./ExpensesFilter.css";
 
-import './ExpensesFilter.css';
+function ExpensesFilter(props) {
+  const selectedYearHandler = (event) => {
+    props.onYearSelected(event.target.value);
+  };
 
-function ExpensesFilter () {
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+        <select value={props.selected} onChange={selectedYearHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
   );
-};
+}
 
 export default ExpensesFilter;
