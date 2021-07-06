2b39a58f433b51c3dcd6069e393642b5d73a7510
Steven_Klavins <stevenklavins94@gmail.com>
2021-07-06 07:54:10 +0100

    chore: wrapped items in li tags

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExspenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-
   const [title, setTitle] = useState(props.title);
 
-  function clickHandler () {
-    setTitle("Test")
-  };
+  function clickHandler() {
+    setTitle("Test");
+  }
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
 
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">£{props.amount} </div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">£{props.amount} </div>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+      </Card>
+    </li>
   );
 }
 
