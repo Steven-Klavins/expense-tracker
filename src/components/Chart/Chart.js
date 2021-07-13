7d48a86d1d6384d80f6243ab950f60c932a8d6a8
Steven_Klavins <stevenklavins94@gmail.com>
2021-07-13 23:16:24 +0100

    feat: added chart component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,11 +1,24 @@
 import React from "react";
+import ChartBar from "./ChartBar";
 import "./Chart.css";
+import { v4 as uuidv4 } from "uuid";
 
-function Chart() {
-  return 
-  <div>
+function Chart(props) {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMax = Math.max(...dataPointValues);
 
-  </div>;
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          value={dataPoint.value}
+          maxValue={totalMax}
+          label={dataPoint.label}
+          key={uuidv4()}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Chart;
